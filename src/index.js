ae4897bd05a5b304ec79bd4843f6606130d18e6e
Vitkor Chernysh <viktor.chernysh.work@gmail.com>
2021-08-07 20:14:28 +0300

    add theme switch

--- src/index.js
+++ src/index.js
@@ -3,10 +3,42 @@ import dataTemplate from './menu.json';
 
 
 const menuRef = document.querySelector('.js-menu');
+const switcherThemeRef = document.querySelector('#theme-switch-toggle');
 
 function createMenuMarkup(dataTemplate) {
   return dataTemplate.map(markupTemplate).join('')
 };
 
-const menuMarkup = createMenuMarkup(dataTemplate);
-menuRef.insertAdjacentHTML('beforeend', menuMarkup)
+
+menuRef.insertAdjacentHTML('beforeend', createMenuMarkup(dataTemplate));
+
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+setCurrentTheme();
+function setCurrentTheme() {
+  const theme = localStorage.getItem('theme');
+  if (theme === null) {
+    return
+  }
+  if (theme === Theme.DARK) {
+    switcherThemeRef.checked = true;
+    document.body.classList.add(Theme.DARK);
+   }
+ }
+
+
+switcherThemeRef.addEventListener('change', themeSwitcher)
+
+function themeSwitcher(e) {
+
+  if (e.target.checked) {
+    localStorage.setItem('theme', Theme.DARK);
+    document.body.classList.add(Theme.DARK);
+  } else {
+    localStorage.setItem('theme', Theme.LIGHT);
+    document.body.classList.replace(Theme.DARK, Theme.LIGHT);
+  }
+};
